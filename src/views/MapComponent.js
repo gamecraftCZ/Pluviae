ee9f0220015ee5ae2a8044182b606b83bae5ff7b
Patrik Vácal <gamecraftCZZ@gmail.com>
2019-07-21 04:17:52 +0200

    Added getting water quality measurement from database.

--- src/views/MapComponent.js
+++ src/views/MapComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import L from 'leaflet';
 import Map from "./Map";
+import axios from "axios";
 
 export default class MapComponent extends Component {
     state = {
@@ -9,7 +10,7 @@ export default class MapComponent extends Component {
     };
 
 
-    componentDidMount() {
+    async componentDidMount() {
         // Leaflet map setup
         this.setState(
             {
@@ -31,6 +32,53 @@ export default class MapComponent extends Component {
             }
         );
 
+
+        const url = "https://3247f322-d1ab-44f2-9950-8a8ef2802387-bluemix.cloudantnosqldb.appdomain.cloud/sensors/_all_docs";
+        const auth = {
+            username: "3247f322-d1ab-44f2-9950-8a8ef2802387-bluemix",
+            password: "d9be3c9dee4dca6c97c00b8718af528264302ad33b3b508a6639d17579350e2a",
+        };
+        const params = {
+            include_docs: true,
+        };
+
+        axios.get(url, {auth, params}).then(data => {
+                // console.warn(data);
+
+                let items = data.data.rows;
+                // console.log(items);
+                items = items.map(item => item.doc);
+                console.log(items);
+
+                items.map(item => {
+                    L.marker(item.location).addTo(this.state.map);
+                    let color = "white";
+                    switch(item.waterQuality) {
+                        case 1:
+                        case 2:
+                        case 3:
+                            color = "green";
+                            break;
+                        case 4:
+                        case 5:
+                        case 6:
+                            color = "orange";
+                            break;
+                        case 7:
+                        case 8:
+                        case 9:
+                        case 10:
+                            color = "red";
+                            break;
+                    }
+                    L.polygon(item.boundaries, {color}).addTo(this.state.map);
+
+                    console.log("Added to map: ", item);
+                });
+            }
+        ).catch(error => {
+            console.error("Cant get data, ", error)
+        });
 
     }
 
