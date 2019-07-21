cf768bdd46402da9ce9d828620d5ea44c6b78dc1
Patrik Vácal <gamecraftCZZ@gmail.com>
2019-07-21 13:17:13 +0200

    Updates

---
+++ precache-manifest.302fe213aef619f331514d98664c4a89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0eb24ab6ba8cf012232833041cd301a",
+    "url": "/Pluviae/index.html"
+  },
+  {
+    "revision": "a9e7bdc8bfd4d4f4bf69",
+    "url": "/Pluviae/static/css/main.dfe57d51.chunk.css"
+  },
+  {
+    "revision": "86ffb61346dd4ffdea8e",
+    "url": "/Pluviae/static/js/2.c62a4923.chunk.js"
+  },
+  {
+    "revision": "a9e7bdc8bfd4d4f4bf69",
+    "url": "/Pluviae/static/js/main.2f424eaf.chunk.js"
+  },
+  {
+    "revision": "fc904cb4e856eb465c42",
+    "url": "/Pluviae/static/js/runtime~main.5595a7f0.js"
+  },
+  {
+    "revision": "08bcb7e8887d656d27290b350bf7075f",
+    "url": "/Pluviae/static/media/logo.08bcb7e8.png"
+  },
+  {
+    "revision": "913daeee2ebb843c45e8772d6d725942",
+    "url": "/Pluviae/static/media/settings.913daeee.svg"
+  }
+]);
